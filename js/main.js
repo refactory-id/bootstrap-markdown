adfe7eeded775c1802d79c2f2f75e552a985bd95
Taufan Aditya <toopay@taufanaditya.com>
2013-03-22 01:14:36 +0700

    Markup section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+$(function(){
+	$('button[type="submit"]').click(function(){
+		var form = $(this).parent(),
+			formResult = [],
+			res = $('<pre/>',{
+				'class':'prettyprint'
+			})
+
+		$.each(form.serializeArray(),function(k,v){
+			formResult.push(v.name+' => '+v.value)
+		})
+
+		res.html('\n'+formResult.join('\n\n')+'\n')
+		form.replaceWith(res)
+
+		return false
+	})
+})
