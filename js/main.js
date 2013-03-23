cf9dfffaf6580a8e954e8762029d8e0ab46e72c1
Taufan Aditya <toopay@taufanaditya.com>
2013-03-23 20:44:29 +0700

    Custom buttons examples & doc

--- js/main.js
+++ js/main.js
@@ -20,6 +20,36 @@ $(function(){
 		return false
 	})
 
+	$('#target-editor-with-custom-buttons').markdown({
+		additionalButtons: [
+			[{
+		        name: 'groupCustom',
+		        data: [{
+		          name: 'cmdBeer',
+		          title: 'Beer',
+		          icon: 'icon icon-glass',
+		          callback: function(e){
+		            // Replace selection with some drinks
+		            var chunk, cursor, selected = e.getSelection(), content = e.getContent(),
+		            	drinks = ['Heinekken', 'Budweiser', 'Iron City', 'Amstel Light', 'Red Stripe', 'Smithwicks', 'Westvleteren', 'Sierra Nevada', 'Guinness', 'Corona', 'Calsberg'],
+		            	index = Math.floor((Math.random()*10)+1)
+
+
+	              	// Give random drink
+	              	chunk = drinks[index]
+
+		            // transform selection and set the cursor into chunked text
+	              	e.replaceSelection(chunk)
+		            cursor = selected.start
+
+		            // Set the cursor
+		            e.setSelection(cursor,cursor+chunk.length)
+		          }
+		        }]
+			}]
+		]
+	})
+
 	$('#editor-triger-init').click(function(){
 		$('#target-editor').markdown({
 			savable:true,
