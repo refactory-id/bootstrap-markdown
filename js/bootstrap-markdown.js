9dbc048971cebc23db832c0856accbffcf42946a
toopay <toopay@taufanaditya.com>
2013-10-21 07:32:57 +0700

    Bump version

--- js/bootstrap-markdown.js
+++ js/bootstrap-markdown.js
@@ -1,5 +1,5 @@
 /* ===================================================
- * bootstrap-markdown.js v2.0.0
+ * bootstrap-markdown.js v2.1.0
  * http://github.com/toopay/bootstrap-markdown
  * ===================================================
  * Copyright 2013 Taufan Aditya
