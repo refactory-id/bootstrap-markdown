f0c383049c857b0ea9e7a537ad80374ed1fbffc0
Taufan Aditya <toopay@taufanaditya.com>
2014-06-28 15:03:40 +0700

    Update documentation and add additional sample implementation

--- js/main.js
+++ js/main.js
@@ -46,6 +46,27 @@ $(function(){
 		]
 	})
 
+	$('#target-editor-twitter').markdown({
+		hiddenButtons:'cmdPreview',
+		footer:'<div id="twitter-footer" class="well" style="display:none;"></div><small id="twitter-counter" class="text-success">140 character left</small>',
+		onChange:function(e){
+			var content = e.parseContent(),
+				content_length = (content.match(/\n/g)||[]).length + content.length
+
+			if (content == '') {
+				$('#twitter-footer').hide()
+			} else {
+				$('#twitter-footer').show().html(content)
+			}
+
+			if (content_length > 140) {
+				$('#twitter-counter').removeClass('text-success').addClass('text-danger').html(content_length-140+' character surplus.')
+			} else {
+				$('#twitter-counter').removeClass('text-danger').addClass('text-success').html(140-content_length+' character left.')
+			}
+		}
+	})
+
 	$('#target-editor-with-custom-language').markdown({language:'fr'})
 
 	$('#target-fa-editor').markdown({iconlibrary: 'fa'})
@@ -76,6 +97,9 @@ $(function(){
 				}
 
 				return previewContent
+			},
+			onChange: function(e) {
+				console.log('Changed!')
 			},
 			onSave: function(e) {
 				alert('Saving "'+e.getContent()+'"...')
