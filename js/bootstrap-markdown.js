c0d771d1427ed1211f5bebdf9f0f1de8d409dd26
Taufan Aditya <toopay@taufanaditya.com>
2014-08-20 04:07:05 +0700

    Bump minor version

--- js/bootstrap-markdown.js
+++ js/bootstrap-markdown.js
@@ -1,5 +1,5 @@
 /* ===================================================
- * bootstrap-markdown.js v2.5.0
+ * bootstrap-markdown.js v2.6.0
  * http://github.com/toopay/bootstrap-markdown
  * ===================================================
  * Copyright 2013-2014 Taufan Aditya
