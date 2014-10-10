9596a58a2e9237d4844bb9b3658adf76cd9d2b47
tobias-nitsche <t.nitsche@checkdomain.de>
2014-10-10 10:58:22 +0200

    Added german localization

---
+++ locale/bootstrap-markdown.de.js
@@ -0,0 +1,24 @@
+/**
+ * German translation for bootstrap-markdown
+ * Tobias Nitsche <tobias-nitsche@gmx.net>
+ */
+(function ($) {
+  $.fn.markdown.messages.de = {
+    'Bold': "Fett",
+    'Italic': "Kursiv",
+    'Heading': "Überschrift",
+    'URL/Link': "Link hinzufügen",
+    'Image': "Bild hinzufügen",
+    'List': "Liste",
+    'Preview': "Vorschau",
+    'strong text': "Sehr betonter Text",
+    'emphasized text': "Betonter Text",
+    'heading text': "Überschrift Text",
+    'enter link description here': "Linkbeschreibung",
+    'Insert Hyperlink': "URL",
+    'enter image description here': "Bildbeschreibung",
+    'Insert Image Hyperlink': "Bild-URL",
+    'enter image title here': "Titel des Bildes",
+    'list text here': "Aufzählungs-Text"
+  };
+}(jQuery));
