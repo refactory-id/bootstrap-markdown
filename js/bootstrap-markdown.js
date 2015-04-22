102f0bd603075e733ff367b46ab5ebda029d3d7b
Taufan Aditya <toopay@taufanaditya.com>
2015-04-22 18:29:57 +0700

    Version bump

--- js/bootstrap-markdown.js
+++ js/bootstrap-markdown.js
@@ -1,5 +1,5 @@
 /* ===================================================
- * bootstrap-markdown.js v2.8.0
+ * bootstrap-markdown.js v2.9.0
  * http://github.com/toopay/bootstrap-markdown
  * ===================================================
  * Copyright 2013-2015 Taufan Aditya
