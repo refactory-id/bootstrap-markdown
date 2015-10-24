f7476b75264c85abd517f80abdd42b37f267d48e
Magicalex <magicalex@mondedie.fr>
2015-10-24 20:50:33 +0200

    add some translation fr

--- locale/bootstrap-markdown.fr.js
+++ locale/bootstrap-markdown.fr.js
@@ -12,13 +12,20 @@
     'List': "Liste à puces",
     'Preview': "Prévisualiser",
     'strong text': "texte important",
-    'emphasized text': "texte souligné",
+    'emphasized text': "texte en italique",
     'heading text': "texte d'entête",
     'enter link description here': "entrez la description du lien ici",
     'Insert Hyperlink': "Insérez le lien hypertexte",
     'enter image description here': "entrez la description de l'image ici",
     'Insert Image Hyperlink': "Insérez le lien hypertexte de l'image",
     'enter image title here': "entrez le titre de l'image ici",
-    'list text here': "texte à puce ici"
+    'list text here': "texte à puce ici",
+    'Save': "Sauvegarder",
+    'Ordered List': "Liste ordonnée",
+    'Unordered List': "Liste désordonnée",
+    'Quote': "Citation",
+    'quote here': "Votre citation",
+    'Code': "Code",
+    'code text here': "écrire du code ici"
   };
 }(jQuery));
