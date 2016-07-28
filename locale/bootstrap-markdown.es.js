b268031abd9ac54071361d9a020615a405715c71
Javier <javierchacon24@gmail.com>
2016-07-28 18:51:45 -0400

    Update bootstrap-markdown.es.js
    Extend Spanish translation.

--- locale/bootstrap-markdown.es.js
+++ locale/bootstrap-markdown.es.js
@@ -10,15 +10,22 @@
     'URL/Link': "Inserte un link",
     'Image': "Inserte una imagen",
     'List': "Lista de items",
+    'Unordered List': "Lista desordenada",
+    'Ordered List': "Lista ordenada",
+    'Code': "Código",
+    'Quote': "Cita",
     'Preview': "Previsualizar",
-    'strong text': "texto importante",
-    'emphasized text': "texto con énfasis",
-    'heading text': "texto titular",
-    'enter link description here': "descripción del link",
+    'strong text': "Texto importante",
+    'emphasized text': "Texto con énfasis",
+    'heading text': "Texto de título",
+    'enter link description here': "Descripción del link",
     'Insert Hyperlink': "Inserte un hipervínculo",
-    'enter image description here': "descripción de la imagen",
+    'enter image description here': "Descripción de la imagen",
     'Insert Image Hyperlink': "Inserte una imagen con un hipervínculo",
     'enter image title here': "Inserte una imagen con título",
-    'list text here': "lista con texto"
+    'list text here': "Texto de lista aquí",
+    'code text here': "Código aquí",
+    'quote here': "Cita aquí",
+    'Save': "Guardar"
   };
 }(jQuery));
