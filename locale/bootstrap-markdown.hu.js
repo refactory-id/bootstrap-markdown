9cea4c1c42aabeb4d76427ab4a579cf0555ae723
Nagy Szilveszter <nagy.szilveszter@bitandpixel.hu>
2017-03-22 11:25:39 +0100

    Added hungarian locale.

---
+++ locale/bootstrap-markdown.hu.js
@@ -0,0 +1,26 @@
+/**
+ * Hungarian translation for bootstrap-markdown
+ * T1k3 <t1k3hu@gmail.com>
+ */
+(function ($) {
+  $.fn.markdown.messages.hu = {
+    'Bold': 'Félkövér',
+    'Italic': 'Dőlt',
+    'Heading': 'Címsor',
+    'URL/Link': 'URL/Link',
+    'Image': 'Kép',
+    'List': 'Lista',
+    'Preview': 'Előnézet',
+    'strong text': 'Félkövér',
+    'emphasized text': 'Kiemelt szöveg',
+    'heading text': 'Címszöveg',
+    'enter link description here': 'Adja meg a link leírását',
+    'Insert Hyperlink': 'URL beszúrása',
+    'enter image description here': 'Adja meg a kép leírását',
+    'Insert Image Hyperlink': 'Kép URL beszúrása',
+    'enter image title here': 'Adja meg a kép címét',
+    'list text here': 'Lista szövege',
+    'quote here': 'Idézet',
+    'code text here': 'Kód'
+  };
+}(jQuery));
