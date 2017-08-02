f3754636b7fb154a69a093ac0d06a1622720f6f1
awebr000 <mas.awe.awe@gmail.com>
2017-08-02 11:08:21 +0700

    add btn-default class again

--- js/bootstrap-markdown.js
+++ js/bootstrap-markdown.js
@@ -122,9 +122,9 @@
 
             // Construct the button object
             buttonContainer = $('<button></button>');
-            buttonContainer.text(' ' + this.__localize(btnText)).addClass('btn-secondary btn-sm').addClass(btnClass);
+            buttonContainer.text(' ' + this.__localize(btnText)).addClass('btn-default btn-secondary btn-sm').addClass(btnClass);
             if (btnClass.match(/btn\-(primary|success|info|warning|danger|link)/)) {
-              buttonContainer.removeClass('btn-secondary');
+              buttonContainer.removeClass('btn-default btn-secondary');
             }
             buttonContainer.attr({
               'type': 'button',
