75731a1ddd92d6c68f096ac501cb59c8017da379
RobiNN <RobiNN1@users.noreply.github.com>
2018-07-25 23:22:25 +0200

    Create bootstrap-markdown.sk.js

---
+++ locale/bootstrap-markdown.sk.js
@@ -0,0 +1,27 @@
+/**
+ * Slovak translation for bootstrap-markdown
+ * RobiNN <kelcakrobo@gmail.com>
+ */
+(function ($) {
+  $.fn.markdown.messages.sk = {
+    'Bold': "Tučne",
+    'Italic': "Kurzíva",
+    'Heading': "Nadpis",
+    'URL/Link': "URL/Odkaz",
+    'Image': "Obrázok",
+    'Unordered List': "Zoznam",
+    'Ordered List': "Zoradený zoznam",
+    'Code': "Úsek kódu",
+    'Quote': "Citácia",
+    'Preview': "Náhľad",
+    'strong text': "tučný text",
+    'emphasized text': "zdôraznený text",
+    'heading text': "text nadpisu",
+    'enter link description here': "sem vlož popis odkazu",
+    'Insert Hyperlink': "Vložiť odkaz",
+    'enter image description here': "sem vlož popis obrázku",
+    'Insert Image Hyperlink': "Vlož adresu obrázka",
+    'enter image title here': "sem vlož nadpis obrázku",
+    'list text here': "položka zoznamu"
+  };
+}(jQuery));
