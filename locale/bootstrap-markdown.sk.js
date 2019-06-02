70253dca857929e406f8d187e032b82feeaebc43
Jakub ADAMEC <info@jadamec.sk>
2019-06-02 20:40:36 +0200

    Create bootstrap-markdown.sk.js
    Add Slovak translation

---
+++ locale/bootstrap-markdown.sk.js
@@ -0,0 +1,27 @@
+/**
+ * Slovak translation for bootstrap-markdown
+ * Jakub ADAMEC <info@jadamec.sk>
+ */
+(function ($) {
+  $.fn.markdown.messages.cs = {
+    'Bold': "Tučné",
+    'Italic': "Kurzíva",
+    'Heading': "Nadpis",
+    'URL/Link': "URL/Odkaz",
+    'Image': "Obrázok",
+    'Unordered List': "Odrážkový zoznam",
+    'Ordered List': "Číslovaný zoznam",
+    'Code': "Kód",
+    'Quote': "Citát",
+    'Preview': "Náhľad",
+    'strong text': "tučný text",
+    'emphasized text': "zdôraznený text",
+    'heading text': "text nadpisu",
+    'enter link description here': "zadajte text odkazu",
+    'Insert Hyperlink': "Zadajte URL odkazu",
+    'enter image description here': "zadajte text obrázka",
+    'Insert Image Hyperlink': "Zadajte URL obrázka",
+    'enter image title here': "zadajte popis obrázka",
+    'list text here': "položka zoznamu"
+  };
+}(jQuery));
